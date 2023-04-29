a4584dfced2821628d89fa8cc1242168076a9b53
Princeduke223 <122835329+Princeduke223@users.noreply.github.com>
2023-04-29 03:50:18 +0300

    Headers

--- shell.h
+++ shell.h
@@ -24,14 +24,6 @@
 #include <errno.h>
 
 
-/* for read/write buffers */
-
-#define READ_BUF_SIZE 1024
-
-#define WRITE_BUF_SIZE 1024
-
-#define BUF_FLUSH -1
-
 
 /* for command chaining */
 
@@ -135,6 +127,14 @@ typedef struct liststr
  * @histcount: the history line number count
 
  */
+
+/* for read/write buffers */
+
+#define READ_BUF_SIZE 1024
+
+#define WRITE_BUF_SIZE 1024
+
+#define BUF_FLUSH -1
 
 typedef struct passinfo
 
@@ -245,27 +245,6 @@ int _putfd(char c, int fd);
 int _putsfd(char *str, int fd);
 
 
-/* toem_string.c */
-
-int _strlen(char *);
-
-int _strcmp(char *, char *);
-
-char *starts_with(const char *, const char *);
-
-char *_strcat(char *, char *);
-
-
-/* toem_string1.c */
-
-char *_strcpy(char *, char *);
-
-char *_strdup(const char *);
-
-void _puts(char *);
-
-int _putchar(char);
-
 
 /* toem_exits.c */
 
@@ -295,6 +274,32 @@ void *_realloc(void *, unsigned int, unsigned int);
 /* toem_memory.c */
 
 int bfree(void **);
+
+
+/* toem_lists1.c */
+
+size_t list_len(const list_t *);
+
+char **list_to_strings(list_t *);
+
+size_t print_list(const list_t *);
+
+list_t *node_starts_with(list_t *, char *, char);
+
+ssize_t get_node_index(list_t *, list_t *);
+
+
+/* toem_vars.c */
+
+int is_chain(info_t *, char *, size_t *);
+
+void check_chain(info_t *, char *, size_t *, size_t, size_t);
+
+int replace_alias(info_t *);
+
+int replace_vars(info_t *);
+
+int replace_string(char **, char *);
 
 
 /* toem_atoi.c */
@@ -376,6 +381,29 @@ int _unsetenv(info_t *, char *);
 
 int _setenv(info_t *, char *, char *);
 
+/* toem_string.c */
+
+int _strlen(char *);
+
+int _strcmp(char *, char *);
+
+char *starts_with(const char *, const char *);
+
+char *_strcat(char *, char *);
+
+
+/* toem_string1.c */
+
+char *_strcpy(char *, char *);
+
+char *_strdup(const char *);
+
+void _puts(char *);
+
+int _putchar(char);
+
+
+
 
 /* toem_history.c */
 
@@ -401,32 +429,6 @@ size_t print_list_str(const list_t *);
 int delete_node_at_index(list_t **, unsigned int);
 
 void free_list(list_t **);
-
-
-/* toem_lists1.c */
-
-size_t list_len(const list_t *);
-
-char **list_to_strings(list_t *);
-
-size_t print_list(const list_t *);
-
-list_t *node_starts_with(list_t *, char *, char);
-
-ssize_t get_node_index(list_t *, list_t *);
-
-
-/* toem_vars.c */
-
-int is_chain(info_t *, char *, size_t *);
-
-void check_chain(info_t *, char *, size_t *, size_t, size_t);
-
-int replace_alias(info_t *);
-
-int replace_vars(info_t *);
-
-int replace_string(char **, char *);
 
 
 #endif
